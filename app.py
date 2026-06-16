44de0f317fe6c340457eb9165fc4750889c06363
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2026-06-16 19:39:14 +0530

    Final Commit

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import ssl
 import requests
 import io
 import matplotlib.pyplot as plt
+import pydeck as pdk
 
 spreadsheet_id = "1myYlsoOTpXPPN9mKfZkEDrX_H5mlAiIPbM0HxA6L0OY"
 ssl._create_default_https_context = ssl._create_unverified_context
@@ -12,7 +13,18 @@ url = f"https://docs.google.com/spreadsheets/d/{spreadsheet_id}/export?format=cs
 response = requests.get(url, verify=False)
 df = pd.read_csv(io.StringIO(response.text))
 
-st.title("AQUI Dashboard")
+def get_aqi_status(aqi):
+    if aqi<=50:
+        return "Good 🟢"
+    elif aqi<=100:
+        return "Moderate 🟡"
+    elif aqi<=150:
+        return "Poor 🟠"
+    else:
+        return "Severe 🔴"
+
+st.title("🌍 India AQI & Weather Dashboard")
+st.markdown("Real-time Air Quality and Weather Analytics Across Indian Cities")
 
 col1, col2 = st.columns(2)
 
@@ -29,7 +41,7 @@ with col2:
         filtered_data["city"].unique()
     )
 
-recordcount,avg_aqi,avg_temp,avg_humidity,avg_uv=st.columns(5)
+recordcount,avg_aqi,aqi_status,avg_temp,avg_humidity=st.columns(5)
 with recordcount:
     st.metric(
         "Total Records",
@@ -40,6 +52,12 @@ with avg_aqi:
     st.metric(
         "Average AQI",
         round(filtered_data["aqi"].mean(),2)
+    )
+
+with aqi_status:
+    aqi = filtered_data["aqi"].mean()
+    st.status(
+        f"AQI Status: {get_aqi_status(aqi)}"
     )
 
 with avg_temp:
@@ -54,58 +72,126 @@ with avg_humidity:
         round(filtered_data["humidity"].mean(),2)
     )
 
-with avg_uv:
-    st.metric(
-        "Average UV",
-        round(filtered_data["uv"].mean(),2)
+bar1, bar2 = st.columns(2)
+with bar1:
+    st.subheader("AQUI Across Cities")
+    st.bar_chart(
+        df.groupby("city")["aqi"].mean()
     )
 
-st.subheader("AQUI Across Cities")
-st.bar_chart(
-    df.groupby("city")["aqi"].mean()
-)
-
-st.subheader("Temperature Across Cities")
-st.bar_chart(
-    df.groupby("city")["temp_c"].mean()
-)
+with bar2:
+    st.subheader("Temperature Across Cities")
+    st.bar_chart(
+        df.groupby("city")["temp_c"].mean()
+    )
 
 st.subheader("Humidity Across Cities")
 st.bar_chart(
     df.groupby("state")["humidity"].mean()
 )
 
-fig, ax = plt.subplots()
+scat1,scat2,scat3 = st.columns(3)
 
-Temp_AQUI = ax.scatter(
-    filtered_data["temp_c"],
-    filtered_data["AQI_IN"],
-    c=filtered_data["AQI_IN"],
-    cmap="RdYlGn_r"
+with scat1:
+    fig, ax = plt.subplots()
+    st.subheader("AQI Vs Temp")
+    Temp_AQUI = ax.scatter(
+        filtered_data["temp_c"],
+        filtered_data["AQI_IN"],
+        c=filtered_data["AQI_IN"],
+        cmap="RdYlGn_r"
+    )
+
+    plt.colorbar(Temp_AQUI)
+    st.pyplot(fig)
+
+with scat2:
+    fig1, ax1 = plt.subplots()
+    st.subheader("AQI Vs Humidity")
+    Wind_AQUI = ax1.scatter(
+        filtered_data["humidity"],
+        filtered_data["AQI_IN"],
+        c=filtered_data["AQI_IN"],
+        cmap="viridis"
+    )
+    plt.colorbar(Wind_AQUI)
+    st.pyplot(fig1)
+
+with scat3:
+    fig2, ax2 = plt.subplots()
+    st.subheader("AQI Vs UV")
+    UV_AQUI = ax2.scatter(
+        filtered_data["uv"],
+        filtered_data["AQI_IN"],
+        c=filtered_data["AQI_IN"],
+        cmap="viridis"
+    )
+    plt.colorbar(UV_AQUI)
+    st.pyplot(fig2)
+
+
+
+top_polluted = (
+    df.sort_values(
+        by="AQI_IN",
+        ascending=False
+    )
+    .head(10)
 )
 
-plt.colorbar(Temp_AQUI)
-st.pyplot(fig)
-
-fig1, ax1 = plt.subplots()
-Wind_AQUI = ax1.scatter(
-    filtered_data["humidity"],
-    filtered_data["AQI_IN"],
-    c=filtered_data["AQI_IN"],
-    cmap="viridis"
+top_high_temp=(
+    df.sort_values(
+        by="temp_c",
+        ascending=False
+    ).head(10)
 )
-plt.colorbar(Wind_AQUI)
-st.pyplot(fig1)
 
-fig2, ax2 = plt.subplots()
-UV_AQUI = ax2.scatter(
-    filtered_data["uv"],
-    filtered_data["AQI_IN"],
-    c=filtered_data["AQI_IN"],
-    cmap="viridis"
+high_wind = (
+    df.sort_values(
+        by="wind_kph",
+        ascending=False
+    ).head(10)
 )
-plt.colorbar(UV_AQUI)
-st.pyplot(fig2)
+
+tab1,tab2 = st.columns(2)
+
+with tab1:
+    st.subheader("Top 10 Polluted Cities")
+
+    st.dataframe(
+        top_polluted[["city","AQI_IN","pm25","pm10"]]
+    )
+
+with tab2:
+    st.subheader("Top 10 Hottest Cities")
+    st.dataframe(
+        top_high_temp[["city", "temp_c", "feels_like_c"]]
+    )
+
+st.subheader("Top strong winds blown")
+st.dataframe(
+    high_wind[["city","wind_kph","wind_degree","wind_dir"]]
+)
+
+st.pydeck_chart(
+    pdk.Deck(
+        initial_view_state=pdk.ViewState(
+            latitude=df["latitude"].mean(),
+            longitude=df["longitude"].mean(),
+            zoom=4,
+            pitch=50,
+        ),
+        layers=[
+            pdk.Layer(
+                "HeatmapLayer",
+                data=df,
+                get_position=["longitude", "latitude"],
+                get_weight="AQI_IN",
+                opacity=0.8,
+            )
+        ],
+    )
+)
 
 st.header("Raw Data")
 st.dataframe(filtered_data)
